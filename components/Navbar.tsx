4a65145e6f7427e29d20a69bd091ad3f98bdfb72
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-10-04 19:05:41 +0630

    OAuth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,13 +9,27 @@ import { IoMdAdd } from 'react-icons/io';
 import Logo from '../utils/Logo.png';
 
 const Navbar = () => {
+
+  const user = false;
+
   return (
-    <div className="items-center justify-between w-full px-4 py-2 border-b-2 border-gray-200">
+    <div className="flex items-center justify-between w-full px-4 py-2 border-b-2 border-gray-200">
       <Link href="/" >
         <div className="w-[100px] md:w-[130px]">
           <Image className='cursor-pointer' src={Logo} alt="TikTik" layout='responsive'/>
         </div>
       </Link>
+      <div>Search</div>
+      <div>
+        { user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log('Error')}
+          />
+        )}
+      </div>
     </div>
   )
 }
